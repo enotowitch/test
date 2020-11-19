4fbb9a88e973880d45f77dfa5a203a093d1d46ea
enotowitch <en.enotowitch@gmail.com>
2020-11-19 19:21:43 +0200

    if selected tags in POST JOB = 0 alert CHOOSE 3 TAGS

--- js/main.js
+++ js/main.js
@@ -84,11 +84,22 @@ jQuery(document).ready(function () {
 		var choice2 = $('.chosen-choices').find('.search-choice:nth-child(2) span').text();
 		var choice3 = $('.chosen-choices').find('.search-choice:nth-child(3) span').text();
 		// alert(choice1 + choice2 + choice3);
-		$('.main-cards-inner').prepend('<div class="card card-main"><div class="card__job-title">' + postJobTitle + '</div><div class="card__company-name">' + postJobCompanyName + '</div><ul><li class="card-option__salary">' + postJobOptionSalary + '</li><li class="card-option__exp">' + postJobOptionExp + '</li><li class="card-option__location">' + postJobOptionLocation + '</li><li class="card-option__duration">' + postJobOptionDuration + '</li><li class="card-option__workload">' + postJobOptionWorkload + '</li></ul><div class="card__tags"><div class="card__tag added-tag">' + choice1 + '</div><div class="card__tag added-tag">' + choice2 + '</div><div class="card__tag added-tag">' + choice3 + '</div></div><div class="card__icons card-icons"><img class="delete icon-scale" src="img/icons/delete.svg"><img class="like icon-scale" src="img/icons/like.svg"><img class="apply icon-scale" src="img/icons/apply.svg"></div></div>');
+		$('.main-cards-inner').prepend(function () {
+			// ! if selected tags in POST JOB = 0 alert CHOOSE 3 TAGS
+			var chosenTags = $('#post-job-form').find('.chosen-choices li').not('.search-field').length;
+			console.log(chosenTags);
+			if (chosenTags < 3) {
+				alert('PLEASE CHOOSE 3 TAGS')
+				// $('#post-job-form').find('.chosen-search-input').attr('value', 'PLEASE CHOOSE 3 TAGS').attr('style','width: 150px');
+			}
+			else {
+				return '<div class="card card-main"><div class="card__job-title">' + postJobTitle + '</div><div class="card__company-name">' + postJobCompanyName + '</div><ul><li class="card-option__salary">' + postJobOptionSalary + '</li><li class="card-option__exp">' + postJobOptionExp + '</li><li class="card-option__location">' + postJobOptionLocation + '</li><li class="card-option__duration">' + postJobOptionDuration + '</li><li class="card-option__workload">' + postJobOptionWorkload + '</li></ul><div class="card__tags"><div class="card__tag added-tag">' + choice1 + '</div><div class="card__tag added-tag">' + choice2 + '</div><div class="card__tag added-tag">' + choice3 + '</div></div><div class="card__icons card-icons"><img class="delete icon-scale" src="img/icons/delete.svg"><img class="like icon-scale" src="img/icons/like.svg"><img class="apply icon-scale" src="img/icons/apply.svg"></div></div>'
+			}
+		});
 		// RESET
 		$('#post-job-form').trigger("reset");
 		// ! test
-		// todo
+
 
 		// ? test
 
